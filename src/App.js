692ca1e9bd0fd747135606749ab8f6c32619addb
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-03-12 18:31:21 +0000

    Finish building the Main Layout of the web app including the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-
+import MainLayout from './Layouts/MainLayout/MainLayout'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Noise Exposure Web Application
-          </p>
-        </header>
+        <MainLayout/>
       </div>
     );
   }
