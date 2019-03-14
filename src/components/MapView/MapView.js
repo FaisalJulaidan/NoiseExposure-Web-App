ff56068cead607e01675fb8a9674a95156049e7e
Faisal Julaidan <julaidanfa@cardiff.ac.uk>
2019-03-14 15:07:36 +0000

    Revert "when a marker is open and you click on the map, the current marker closes"
    This reverts commit c5b838cffc206c58d59f9f4a5a66202786c0f7ef

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,9 +1,6 @@
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React from 'react';
 
-//used https://www.npmjs.com/package/google-maps-react
-// and https://itnext.io/google-maps-react-makes-adding-google-maps-api-to-a-react-app-a-breeze-effb7b89e54
-
 const mapStyles = {
         width: '100%',
         height: '100%',
@@ -20,26 +17,22 @@ export class MapView extends React.Component{
             {
                 name: 'Principality Stadium',
                 position: { lat: 51.4782, lng: -3.1826},
-                description: 'This is the Principality Stadium',
-                dB_Level: '90dB'     
+                description: 'This is the Principality Stadium'
             },
             {
                 name: 'Cardiff Council',
                 position: { lat: 51.4694, lng: -3.1625},
-                description: 'This is the Cardiff Council',
-                dB_Level: '50dB'   
+                description: 'This is the Cardiff Council'
             },
             {
                 name: 'Cardiff University',
                 position: { lat: 51.4866, lng: -3.1789},
-                description: 'This is the Cardiff University',
-                dB_Level: '73dB'   
+                description: 'This is the Cardiff University'
             },
             {
                 name: 'Cardiff City Football Club',
                 position: { lat: 51.4728, lng: -3.2030},
-                description: 'This is the Cardiff City Football Club',
-                dB_Level: '61dB'   
+                description: 'This is the Cardiff City Football Club'
             }
         ],
           activeMarker: {},
@@ -53,13 +46,13 @@ export class MapView extends React.Component{
           showingInfoWindow: true
         });
       }
-    onMapClick = () => {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-            showingInfoWindow: false,
-            activeMarker: null
-            });
-        }
+    onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+        this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+        });
+    }
     }
     render() {
         return (
@@ -72,14 +65,13 @@ export class MapView extends React.Component{
                             lng: -3.179090
                         }}
                         zoom={14}
-                        onClick={this.onMapClick}>
+                        onClick={this.onMapClicked}>
                         {this.state.markers.map((marker, key) => (
                             <Marker
                                 key={key}
                                 name={marker.name}
                                 description={marker.description}
                                 position={marker.position}
-                                dB_Level={marker.dB_Level}
                                 onClick={this.onMarkerClick} >
                             </Marker> 
                         ))}
@@ -88,7 +80,7 @@ export class MapView extends React.Component{
                             visible = { this.state.showingInfoWindow }>
                             <h1>{this.state.selectedPlace.name}</h1>
                             <p>{this.state.selectedPlace.description}</p>
-                            <p>Noise Level: {this.state.selectedPlace.dB_Level}</p>
+                            <p>50 dB</p>
                         </InfoWindow>
                     </Map>
             </div>
