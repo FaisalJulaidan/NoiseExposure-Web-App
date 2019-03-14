1b1673e356d873e1872976801418e3bb317f2ea7
Joey Gmaj <joeygmaj@hotmail.co.uk>
2019-03-14 13:23:34 +0000

    rewrote component, includes markers and info pop ups on map

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,38 +1,96 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from 'react';
 
 const mapStyles = {
         width: '70%',
-        height: '60vh'
+        height: '80%',
+        marginLeft: "auto",
+        marginRight: "auto",
+        border: '3px solid',
+        borderColor: '#018A99'
     };
 
-const MapComponent = ({ text }) => <div>{text}</div>;
-export default class MapView extends Component{
-    
-    static defaultProps  = {
-        centre: {
-            lat: 51.48,
-            lng: -3.17
-        },
-        zoom: 13
+export class MapView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          showingInfoWindow: false,
+          markers: [
+            {
+                name: 'Principality Stadium',
+                position: { lat: 51.4782, lng: -3.1826},
+                description: 'This is the Principality Stadium'
+            },
+            {
+                name: 'Cardiff Council',
+                position: { lat: 51.4694, lng: -3.1625},
+                description: 'This is the Cardiff Council'
+            },
+            {
+                name: 'Cardiff University',
+                position: { lat: 51.4866, lng: -3.1789},
+                description: 'This is the Cardiff University'
+            },
+            {
+                name: 'Cardiff City Football Club',
+                position: { lat: 51.4728, lng: -3.2030},
+                description: 'This is the Cardiff City Football Club'
+            }
+        ],
+          activeMarker: {},
+          selectedPlace: {}
+        }
     }
-    render(){
-        return(
+    onMarkerClick = (props, marker, e) => {
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+      }
+    onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+        this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+        });
+    }
+    }
+    render() {
+        return (
             <div>
-                <p>Map</p>
-                    <div style={mapStyles}>
-                        <GoogleMapReact
-                        bootstrapURLKeys={{key: 'AIzaSyBAz_fOHTtaJ8spTq2h5kiGtSP-GCBDEEc'}}
-                        defaultCenter={this.props.centre}
-                        defaultZoom={this.props.zoom}
-                        >
-                        <MapComponent
-                            lat = {51.481583}
-                            lng = {-3.179090}
-                            />
-                        </GoogleMapReact>
+                <div>
+                    <Map 
+                        google={this.props.google}
+                        style = {mapStyles}
+                        initialCenter={{
+                            lat: 51.481583,
+                            lng: -3.179090
+                        }}
+                        zoom={14}
+                        onClick={this.onMapClicked}>
+                        {this.state.markers.map((marker, key) => (
+                            <Marker
+                                key={key}
+                                name={marker.name}
+                                description={marker.description}
+                                position={marker.position}
+                                onClick={this.onMarkerClick} >
+                            </Marker> 
+                        ))}
+                        <InfoWindow
+                            marker = { this.state.activeMarker }
+                            visible = { this.state.showingInfoWindow }>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                            <p>{this.state.selectedPlace.description}</p>
+                            <p>50 dB</p>
+                        </InfoWindow>
+                    </Map>
                 </div>
             </div>
         );
-    }
+      }
 }
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyBAz_fOHTtaJ8spTq2h5kiGtSP-GCBDEEc')
+  })(MapView)
