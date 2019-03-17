8940191555af007deb68c600c2ffab1d5fce83a0
Ieuan Parker <Parkeri5@cardiff.ac.uk>
2019-03-17 18:38:49 +0000

    PAIRED: Added Switch that brings up Login Modal

--- src/Layouts/MainLayout/MainLayout.js
+++ src/Layouts/MainLayout/MainLayout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu} from 'antd';
 import styles from './MainLayout.module.css';
 import TableView from '../../components/TableView/TableView'
 import MapView from '../../components/MapView/MapView';
 import {Route, Switch} from 'react-router-dom';
 import {history} from "../../utilities";
+import Login from '../../components/Login/Login'
 
 const { Header, Content, Footer } = Layout;
 
@@ -26,12 +27,12 @@ const MainLayout = (props) => {
                     className={styles.Menu}
                     onClick={handleMenuClick}
                 >
-                    <Menu.Item key="/">Table View</Menu.Item>
+                    <Menu.Item key="/tableView">Table View</Menu.Item>
                     <Menu.Item key="/mapView">Map</Menu.Item>
                     <Menu.Item key="/about">About</Menu.Item>
                 </Menu>
             </Header>
-
+            <Login />
 
             <Content className={styles.Content}>
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import styles from '../../Layouts/MainLayout/MainLayout.module.css';
+import { Modal, Switch, Form, Icon, Input, Button, Checkbox } from 'antd';
+//import Form from "antd/lib/form/Form";
+class Login extends React.Component {
+    state = { visible: false }
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div>
+                <Switch checkedChildren={"View public data"} unCheckedChildren={"View Your own Data"} type="primary" onClick={this.showModal} className={styles.Switch}>
+                    Show Your Own Data
+                </Switch>
+                <Modal
+                    title="Login"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(
+                                <Checkbox>Remember me</Checkbox>
+                            )}
+                            <a className="login-form-forgot" href="">Forgot password</a>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                            Or <a href="">register now!</a>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </div>
+
+        );
+    }
+}
+const WrappedNormalLoginForm = Form.create({ name: 'horizontal_login' })(Login);
+export default WrappedNormalLoginForm;
+
